bd5ff461f1e0b2083e10e1782952edf7745936f0
Dario Curreri <dariocurr@gmail.com>
2021-02-06 23:34:33 +0100

    nothing special

--- confroid/app/src/main/java/fr/uge/confroid/services/ConfigurationPusher.java
+++ confroid/app/src/main/java/fr/uge/confroid/services/ConfigurationPusher.java
@@ -1,4 +1,16 @@
 package fr.uge.confroid.services;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 public class ConfigurationPusher {
+
+    public void pushConfiguration(Intent intent) {
+
+        String name = intent.getStringExtra("name");
+        Bundle bundle = intent.getBundleExtra("content");
+        String tag =intent.getStringExtra("tag");
+
+    };
+
 }
