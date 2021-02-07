42367d322b0838f32c2c5be5b4c0a3466f35d299
Domenico Di Fina <difinadomenico98@gmail.com>
2021-02-07 09:18:04 +0100

    add methods to read and write on SQLite DB

--- confroid/app/src/main/java/fr/uge/confroid/utlis/ConfroidUtils.java
+++ confroid/app/src/main/java/fr/uge/confroid/utlis/ConfroidUtils.java
@@ -1,8 +1,16 @@
 package fr.uge.confroid.utlis;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import fr.uge.confroid.sqlite.ConfroidContract;
+import fr.uge.confroid.sqlite.ConfroidDbHelper;
+import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -51,11 +59,51 @@ public interface ConfroidUtils {
     }
 
     default void saveConfiguration (Context context, String name, Object value, String versionName) {
+        ConfroidDbHelper dbHelper = new ConfroidDbHelper(context);
 
+        // Gets the data repository in write mode
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(ConfroidContract.ConfroidEntry.COLUMN_NAME_NAME, name);
+
+        HashMap<String, Object> content = new HashMap<String,Object>((HashMap)value);
+        values.put(ConfroidContract.ConfroidEntry.COLUMN_NAME_CONTENT, new JSONObject(content).toString());
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(ConfroidContract.ConfroidEntry.TABLE_NAME, null, values);
     }
 
     default <T> void loadConfiguration (Context context, String name, String version, Consumer<T> callback) {
+        ConfroidDbHelper dbHelper = new ConfroidDbHelper(context);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                BaseColumns._ID,
+                ConfroidContract.ConfroidEntry.COLUMN_NAME_NAME,
+                ConfroidContract.ConfroidEntry.COLUMN_NAME_CONTENT
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        String selection = ConfroidContract.ConfroidEntry.COLUMN_NAME_NAME + " = ?";
+        String[] selectionArgs = { name +  "/" + version };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                ConfroidContract.ConfroidEntry.COLUMN_NAME_NAME + " DESC";
+
+        Cursor cursor = db.query(
+                ConfroidContract.ConfroidEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
     }
 
     default <T> void subscribeConfiguration (Context context, String name, Consumer <T> callback) {
