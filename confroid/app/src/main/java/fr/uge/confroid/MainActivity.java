b8fbc5dd4d354a429071f162b0f07cd6318ed07d
Dario Curreri <dariocurr@gmail.com>
2021-02-13 12:18:47 +0100

    trying backup

--- confroid/app/src/main/java/fr/uge/confroid/MainActivity.java
+++ confroid/app/src/main/java/fr/uge/confroid/MainActivity.java
@@ -109,7 +109,7 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        //super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1) {
             if (resultCode == RESULT_OK) {
                 try {
