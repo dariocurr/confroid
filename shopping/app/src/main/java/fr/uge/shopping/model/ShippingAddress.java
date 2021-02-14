678be7d921dbf9aa47ce673409ce9eadbdc99dc9
Dario Curreri <dariocurr@gmail.com>
2021-02-14 18:58:44 +0100

    update shopping project

---
+++ shopping/app/src/main/java/fr/uge/shopping/model/ShippingAddress.java
@@ -0,0 +1,10 @@
+package fr.uge.shopping.model;
+
+public class ShippingAddress {
+    public String name;
+    public String street;
+    public String city;
+    public String country;
+
+    // TODO: constructor
+}
