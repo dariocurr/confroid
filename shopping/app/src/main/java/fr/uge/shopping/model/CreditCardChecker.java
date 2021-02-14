0677bfffcfe12994c1d76e6efb246b1742946da0
Dario Curreri <dariocurr@gmail.com>
2021-02-14 22:31:22 +0100

    credit card checker realized

--- shopping/app/src/main/java/fr/uge/shopping/model/CreditCardChecker.java
+++ shopping/app/src/main/java/fr/uge/shopping/model/CreditCardChecker.java
@@ -9,8 +9,20 @@ import java.util.function.Predicate;
 public class CreditCardChecker implements Predicate<String> {
 
     @Override
-    public boolean test(String cardNumber) {
-        return true;
+    public boolean test(String creditCardNumber) {
+        int n = creditCardNumber.length() - 1;
+        int sum = Integer.parseInt(creditCardNumber.substring(n));
+        for (int i = n - 1; i >= 0; i--) {
+            int number = Integer.parseInt(creditCardNumber.substring(i, i + 1));
+            if ((n - i) % 2 == 1) {
+                number *= 2;
+                if (number > 9) {
+                    number -= 9;
+                }
+            }
+            sum += number;
+        }
+        return (sum % 10 == 0);
     }
 
 }
