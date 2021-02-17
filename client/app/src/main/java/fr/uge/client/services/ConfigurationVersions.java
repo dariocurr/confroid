4bb3326b74ed9b097ebd0139a4ea18cfd3ffc5c9
Domenico Di Fina <difinadomenico98@gmail.com>
2021-02-17 09:24:30 +0100

    fix load all versions bug

--- client/app/src/main/java/fr/uge/client/services/ConfigurationVersions.java
+++ client/app/src/main/java/fr/uge/client/services/ConfigurationVersions.java
@@ -12,6 +12,7 @@ public class ConfigurationVersions extends Service {
     public int onStartCommand(Intent incomingIntent, int flags, int startId) {
         Intent outComingIntent = new Intent(this.getApplicationContext(), ConfigurationVersionsActivity.class);
         outComingIntent.putExtra("versions", incomingIntent.getBundleExtra("versions"));
+        outComingIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(outComingIntent);
         return START_NOT_STICKY;
     }
