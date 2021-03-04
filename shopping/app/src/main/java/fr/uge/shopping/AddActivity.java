9cfac1625963f46d0e80c4f384e6993799eb42e4
emanueledomingo <emanu.domingo@gmail.com>
2021-03-04 19:24:01 +0100

    Created activity to add one shopping configuration

---
+++ shopping/app/src/main/java/fr/uge/shopping/AddActivity.java
@@ -0,0 +1,14 @@
+package fr.uge.shopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import fr.uge.shopping.R;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
