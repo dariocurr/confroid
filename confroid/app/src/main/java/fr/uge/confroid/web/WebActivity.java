c0a64a06aa7211b4250d0ca16bd06106dc1eda74
SalvatoreAntoninoGristina <gristinasalvatore14@gmail.com>
2021-03-07 15:46:47 +0100

    created first web configuration

--- confroid/app/src/main/java/fr/uge/confroid/web/WebActivity.java
+++ confroid/app/src/main/java/fr/uge/confroid/web/WebActivity.java
@@ -3,6 +3,9 @@ package fr.uge.confroid.web;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import fr.uge.confroid.R;
+import org.json.JSONException;
+
+import java.net.URISyntaxException;
 
 public class WebActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class WebActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+
+        try {
+            Server server = new Server();
+        } catch (JSONException  e) {
+            e.printStackTrace();
+        }
     }
 }
