4dccfcb9a927ab6e3cc69814bc0422f35976b2a6
SalvatoreAntoninoGristina <gristinasalvatore14@gmail.com>
2021-03-13 13:07:12 +0100

    created LoginActivity and RegisterActivity

---
+++ confroid/app/src/main/java/fr/uge/confroid/web/LoginActivity.java
@@ -0,0 +1,14 @@
+package fr.uge.confroid.web;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import fr.uge.confroid.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
