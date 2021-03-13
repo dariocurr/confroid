8e8e6070c7c8090a47ffe8cc27de53b9892ebfe7
SalvatoreAntoninoGristina <gristinasalvatore14@gmail.com>
2021-03-13 13:05:47 +0100

    created NetworkThread

---
+++ confroid/app/src/main/java/fr/uge/confroid/web/NetworkThread.java
@@ -0,0 +1,24 @@
+package fr.uge.confroid.web;
+
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class NetworkThread implements Runnable {
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public NetworkThread(String json) throws JSONException, IOException, InterruptedException {
+        Server server = new Server(json);
+        server.start();
+        Client client = new Client();
+        String postLogin = client.loginJson("toto","toto");
+        server.saveRequest();
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
