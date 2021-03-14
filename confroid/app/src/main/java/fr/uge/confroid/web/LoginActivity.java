a6f7fd604dd28391b613fb923c9bb90e15b30738
SalvatoreAntoninoGristina <gristinasalvatore14@gmail.com>
2021-03-14 12:22:06 +0100

    update LoginActivity and RegisterActivity

--- confroid/app/src/main/java/fr/uge/confroid/web/LoginActivity.java
+++ confroid/app/src/main/java/fr/uge/confroid/web/LoginActivity.java
@@ -1,14 +1,86 @@
 package fr.uge.confroid.web;
 
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import fr.uge.confroid.MainActivity;
 import fr.uge.confroid.R;
+import fr.uge.confroid.utlis.ConfroidManagerUtils;
+import fr.uge.confroid.utlis.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button registerBtn;
+    Button loginBtn;
+    EditText username;
+    EditText password;
+    boolean auth = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username_login);
+        password = findViewById(R.id.password_login);
+
+        registerBtn = findViewById(R.id.register_page_button);
+        registerBtn.setOnClickListener(b -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        loginBtn = findViewById(R.id.login_button);
+        loginBtn.setOnClickListener(b -> {
+            String usernameText = username.getText().toString();
+            String passwordText = password.getText().toString();
+            if(!usernameText.equals("") && !passwordText.equals("")) {
+                try {
+                    File database = new File(this.getFilesDir(),"database.json");
+                    JSONObject databaseObj = new JSONObject(FileUtils.readFile(database));
+
+                    JSONArray users = databaseObj.getJSONArray("users");
+
+                    for (int i = 0 ; i < users.length(); i++) {
+                        JSONObject user = users.getJSONObject(i);
+                        if(user.getString("username").equals(usernameText) && user.getString("password").equals(passwordText)){
+                            auth = true;
+                        }
+                    }
+
+                    if(!auth){
+                        Toast alert = Toast.makeText(this, "Username or Password Incorrect! Retry", 2000);
+                        alert.show();
+                    }
+                    else{
+                        Intent intent = new Intent(this, MainActivity.class);
+                        intent.putExtra("username", usernameText);
+                        intent.putExtra("password", passwordText);
+                        intent.putExtra("auth", "true");
+                        startActivity(intent);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                Toast alert = Toast.makeText(this, "Make sure to write username and password!", 2000);
+                alert.show();
+            }
+        });
+
+
     }
 }
--- confroid/app/src/main/java/fr/uge/confroid/web/RegisterActivity.java
+++ confroid/app/src/main/java/fr/uge/confroid/web/RegisterActivity.java
@@ -1,14 +1,87 @@
 package fr.uge.confroid.web;
 
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import fr.uge.confroid.R;
+import fr.uge.confroid.utlis.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button registerBtn;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username_register);
+        password = findViewById(R.id.password_register);
+
+        registerBtn = findViewById(R.id.register_button);
+        registerBtn.setOnClickListener(b -> {
+
+            String usernameText = username.getText().toString();
+            String passwordText = password.getText().toString();
+
+            if(!usernameText.equals("") && !passwordText.equals("")){
+                String accountsJson = loadDataFromAssets("database.json");
+                try {
+                    JSONObject accounts = new JSONObject(accountsJson);
+                    JSONArray users = accounts.getJSONArray("users");
+
+                    JSONObject newUser = new JSONObject();
+                    newUser.put("username", usernameText);
+                    newUser.put("password", passwordText);
+
+                    users.put(newUser);
+
+                    File database = new File(this.getFilesDir(),"database.json");
+                    FileUtils.writeFile(database, accounts.toString());
+
+                    Intent intent = new Intent(this, LoginActivity.class);
+                    startActivity(intent);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                Toast alert = Toast.makeText(this, "Make sure to write username and password!", 2000);
+                alert.show();
+            }
+        });
+    }
+
+    public String loadDataFromAssets(String inFile) {
+        String tContents = "";
+
+        try {
+            InputStream stream = getAssets().open(inFile);
+
+            int size = stream.available();
+            byte[] buffer = new byte[size];
+            stream.read(buffer);
+            stream.close();
+            tContents = new String(buffer);
+        } catch (IOException e) {
+            // Handle exceptions here
+        }
+
+        return tContents;
+
     }
 }
