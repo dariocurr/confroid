5673140280e111a7ad05d3a626c9f47fa923270d
arnaud <arnaud.vanspauwen@student.ucll.be>
2021-03-16 14:36:55 +0100

    Subscription docs

--- confroid/app/src/main/java/fr/uge/confroid/services/Subscription.java
+++ confroid/app/src/main/java/fr/uge/confroid/services/Subscription.java
@@ -11,20 +11,36 @@ public class Subscription {
     private final long creationTime;
     private final int expirationTime;
 
+    /**
+     * @param subscriber
+     * @param expirationTime
+     * contructor
+     */
     public Subscription(String subscriber, int expirationTime) {
         this.subscriber = subscriber;
         this.creationTime = System.currentTimeMillis();
         this.expirationTime = expirationTime;
     }
 
+    /**
+     * @param currentTime
+     * @return boolean expired or not
+     */
     public boolean isExpired(long currentTime) {
         return ((currentTime - this.creationTime) / 1000) > this.expirationTime;
     }
 
+    /**
+     * @return give subscriber
+     */
     public String getSubscriber() {
         return subscriber;
     }
 
+    /**
+     * @param o
+     * @return boolean
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -33,6 +49,10 @@ public class Subscription {
         return subscriber.equals(that.subscriber);
     }
 
+    /**
+     * hashcode
+     * @return hash
+     */
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public int hashCode() {
