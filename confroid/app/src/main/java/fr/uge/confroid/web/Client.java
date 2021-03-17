1a0788a3d2244476683ba4b31057d67c1e72c428
arnaud <arnaud.vanspauwen@student.ucll.be>
2021-03-17 17:07:27 +0100

    client

--- confroid/app/src/main/java/fr/uge/confroid/web/Client.java
+++ confroid/app/src/main/java/fr/uge/confroid/web/Client.java
@@ -10,6 +10,12 @@ public class Client {
 
     OkHttpClient client = new OkHttpClient();
 
+    /**
+     * @param url
+     * @param json
+     * @return body string
+     * @throws IOException
+     */
     public String post(HttpUrl url, String json) throws IOException {
         RequestBody body = RequestBody.create(json, JSON);
         Request request = new Request.Builder()
@@ -26,6 +32,11 @@ public class Client {
         return null;
     }
 
+    /**
+     * @param username
+     * @param password
+     * @return json string with name and password
+     */
     String loginJson(String username, String password) {
         return "{'user':["
                 + "{'name':'" + username + "',"
